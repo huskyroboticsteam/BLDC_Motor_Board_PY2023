f6cd7cd940b5b14ae2757a00a8dfb7361f4ee124
lucas2032 <118077761+lucas2032@users.noreply.github.com>
2024-01-25 18:24:40 -0800

    FSM Code Update

--- TestCode.cydsn/BLDC_FSM.c
+++ TestCode.cydsn/BLDC_FSM.c
@@ -1,67 +1,87 @@
 #include "project.h"
 
-/* [] END OF FILE */
+// State of U, V, and W inductors high (polarity) and low (enable)
 uint8_t UHstate = 0;
 uint8_t ULstate = 0;
 uint8_t VHstate = 0;
 uint8_t VLstate = 0;
+uint8_t WHstate = 0;
+uint8_t WLstate = 0;
 
+// Hall effect sensor binary data
+uint8_t hall1State;
+uint8_t hall2State;
+uint8_t hall3State;
+
+// FSM changing through states
 void readHallState() {
+    hall1State = HALL1_Read();
+    hall2State = HALL2_Read();
+    hall3State = HALL3_Read();
+    
 	// State 1 (001)
-	if (~HALL1_read() && ~HALL2_read() && HALL3_read()) {
-		UH = 1
-		UL = 1
-		// VH = X
-		VL = 0
-		WH = 0
-		WL = 1
+	if (~hall1State && ~hall2State && hall3State) {
+        UHstate = 1;
+		ULstate = 1;
+		// VHstate = X;
+		VLstate = 0;
+		WHstate = 0;
+		WLstate = 1;
 	}
 
 	// State 2 (101)
-	if (~HALL1_read() && HALL2_read() && ~HALL3_read()) {
-		// UH = X
-		UL = 0
-		VH = 1
-		VL = 0
-		WH = 0
-		WL = 1
+	if (~hall1State && hall2State && hall3State) {
+		// UHstate = X;
+		ULstate = 0;
+		VHstate = 1;
+		VLstate = 0;
+		WHstate = 0;
+		WLstate = 1;
 	}
 
 	// State 3 (100)
-	if (~HALL1 && HALL2 && HALL3) {
-		UH = 0
-		UL = 1
-		VH = 1
-		VL = 1
-		// WH = X
-		WL = 0
+	if (~hall1State && hall2State && hall3State) {
+		UHstate = 0;
+		ULstate = 1;
+		VHstate = 1;
+		VLstate = 1;
+		// WHstate = X;
+		WLstate = 0;
 	}
 
     // State 4 (110)
-    if (HALL1 && ~HALL2 && ~HALL3) {
-		UH = 0
-		UL = 1
-		// VH = X
-		VL = 0
-		WH = 0
-		WL = 1
+    if (hall1State && ~hall2State && ~hall3State) {
+		UHstate = 0;
+		ULstate = 1;
+		// VHstate = X;
+		VLstate = 0;
+		WHstate = 0;
+		WLstate = 1;
     }
 	    // State 5 (010)
-    if (HALL1 && ~HALL2 && HALL3) {
-		// UH = X
-		UL = 1
-		VH = 0
-		VL = 1
-		WH = 0
-		WL = 1
-
-	    // State 6 (011)
-    if (HALL1 && HALL2 && ~HALL3) {
-		UH = 1
-		UL = 1
-		// VH = X
-		VL = 1
-		WH = 0
-		WL = 1
+    if (hall1State && ~hall2State && hall3State) {
+		// UHstate = X;
+		ULstate = 1;
+		VHstate = 0;
+		VLstate = 1;
+		WHstate = 0;
+		WLstate = 1;
     }
+	    // State 6 (011)
+    if (hall1State && hall2State && ~hall3State) {
+		UHstate = 1;
+		ULstate = 1;
+		// VHstate = X;
+		VLstate = 1;
+		WHstate = 0;
+		WLstate = 1;
+    }
+    
+    UH_Write(UHstate);
+    UL_Write(ULstate);
+    VH_Write(VHstate);
+    VL_Write(VLstate);
+    WH_Write(WHstate);
+    WL_Write(WLstate);
+    
 }
